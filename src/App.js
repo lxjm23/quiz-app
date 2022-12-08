40839136dafeee1bbbf218f9ca5c734bbea4437f
Jm's Laptop <ljm12543@gmail.com>
2022-12-07 23:57:38 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>djhksajkdh</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Navbar(){
+  return(
+  <div>
+    <h1>QuizMe</h1>
+  </div>)
+}
+
+export default Navbar()
